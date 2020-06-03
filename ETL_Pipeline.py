337dcd94e31c3950e6bc66f895de2519f92b57dc
phuongvnguyen <31282453+phuongvnguyen@users.noreply.github.com>
2020-06-03 16:44:14 +0200

    Update ETL_Pipeline.py

--- ETL_Pipeline.py
+++ ETL_Pipeline.py
@@ -1,5 +1,6 @@
 """
-In this program, I present an end-to-end project on the Extract, Transform, and Load (ETL) process. Specification requirements are Pandas, NumPy, and SQLite.
+In this program, I present an end-to-end project on the Extract, Transform, and Load (ETL) process. 
+Specification requirements are Pandas, NumPy, and SQLite.
 
 Programmer: Phuong Van Nguyen
 phuong.nguyen@economics.uni-kiel.de
